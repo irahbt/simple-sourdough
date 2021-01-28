447a3a7402d96167efa9509505554c9fbe0ff841
irahbt <irah.bt@gmail.com>
2021-01-28 15:54:30 +0000

    Add update / remove views for categories

--- basket/views.py
+++ basket/views.py
@@ -65,62 +65,80 @@ def add_to_basket(request, item_id, category):
     return redirect(redirect_url)
 
 
-def update_basket(request, item_id):
+def update_basket(request, item_id, category):
     """Update the quantity of the specified product to the specified amount"""
 
-    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     colour = None
     if 'product_colour' in request.POST:
         colour = request.POST['product_colour']
-    basket = request.session.get('basket', {})
-
-    if colour:
-        if quantity > 0:
-            basket[item_id]['items_by_colour'][colour] = quantity
-            messages.success(
-                    request, f'{colour.capitalize()} {product.name} quantity has been updated to {basket[item_id]["items_by_colour"][colour]}')
+    
+    basket = request.session.get('basket', {'product': {},
+                                            'subscription': {}})
+    if category == 'product':
+        product = get_object_or_404(Product, pk=item_id)
+        if colour:
+            if quantity > 0:
+                basket[category][item_id]['items_by_colour'][colour] = quantity
+                messages.success(
+                        request, f'{colour.capitalize()} {product.name} quantity has been updated to {basket[category][item_id]["items_by_colour"][colour]}')
+            else:
+                del basket[category][item_id]['items_by_colour'][colour]
+                if not basket[category][item_id]['items_by_colour']:
+                    basket.pop(item_id)
+                messages.success(
+                    request, f'{colour.capitalize()} {product.name} has been removed from your basket')
         else:
-            del basket[item_id]['items_by_colour'][colour]
-            if not basket[item_id]['items_by_colour']:
+            if quantity > 0:
+                basket[category][item_id] = quantity
+                messages.success(
+                        request, f'{product.name} quantity has been updated to {basket[category][item_id]}')
+            else:
                 basket.pop(item_id)
-            messages.success(
-                request, f'{colour.capitalize()} {product.name} has been removed from your basket')
+                messages.success(
+                    request, f'{product.name} has been removed from your basket')
 
-    else:
+    elif category == 'subscription':
+        subscription = get_object_or_404(Subscription, pk=item_id)
         if quantity > 0:
-            basket[item_id] = quantity
+            basket[category][item_id] = quantity
             messages.success(
-                    request, f'{product.name} quantity has been updated to {basket[item_id]}')
+                    request, f'{subscription.name} quantity has been updated to {basket[category][item_id]}')
         else:
             basket.pop(item_id)
             messages.success(
-                request, f'{product.name} has been removed from your basket')
+                request, f'{subscription.name} has been removed from your basket')
 
     request.session['basket'] = basket
     return redirect(reverse('view_basket'))
 
 
-def remove_from_basket(request, item_id):
+def remove_from_basket(request, item_id, category):
     """Remove the item from the shopping basket"""
-
+    colour = None
+    if 'product_colour' in request.POST:
+        colour = request.POST['product_colour']
+    basket = request.session.get('basket', {'product': {},
+                                            'subscription': {}})
     try:
-        product = get_object_or_404(Product, pk=item_id)
-        colour = None
-        if 'product_colour' in request.POST:
-            colour = request.POST['product_colour']
-        basket = request.session.get('basket', {})
-
-        if colour:
-            del basket[item_id]['items_by_colour'][colour]
-            if not basket[item_id]['items_by_colour']:
+        if category == 'product':
+            product = get_object_or_404(Product, pk=item_id)
+            if colour:
+                del basket[category][item_id]['items_by_colour'][colour]
+                if not basket[category][item_id]['items_by_colour']:
+                    basket.pop(item_id)
+                messages.success(
+                    request, f'{colour.capitalize()} {product.name} has been removed from your basket')
+            else:
                 basket.pop(item_id)
-            messages.success(
-                request, f'{colour.capitalize()} {product.name} has been removed from your basket')
-        else:
+                messages.success(
+                    request, f'{product.name} has been removed from your basket')
+
+        elif category == 'subscription':
+            subscription = get_object_or_404(Subscription, pk=item_id)
             basket.pop(item_id)
             messages.success(
-                request, f'{product.name} has been removed from your basket')
+                request, f'{subscription.name} has been removed from your basket')
 
         request.session['basket'] = basket
         return HttpResponse(status=200)
