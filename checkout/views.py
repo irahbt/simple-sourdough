3dcfc1be63c874d2c4f613982a1f76b3d3e2828f
irahbt <irah.bt@gmail.com>
2021-02-01 15:19:16 +0000

    Update webhooks for user profiles

--- checkout/views.py
+++ checkout/views.py
@@ -7,7 +7,10 @@ from .forms import OrderForm
 from .models import Order, OrderLineItem, SubscriptionOrderLineItem
 from products.models import Product
 from subscriptions.models import SubscriptionPlan
+from profiles.models import UserProfile
+from profiles.forms import UserProfileForm
 from basket.contexts import basket_contents
+
 
 import stripe
 import json
@@ -90,6 +93,26 @@ def checkout(request):
                             order.delete()
                             return redirect(reverse('view_basket'))
 
+                    elif category == 'subscription':
+                        try:
+                            plan = get_object_or_404(
+                                SubscriptionPlan, pk=item_id)
+                            if isinstance(item_data, int):
+                                order_line_item = SubscriptionOrderLineItem(
+                                    order=order,
+                                    plan=plan,
+                                    quantity=item_data,
+                                )
+                                order_line_item.save()
+    
+                        except SubscriptionPlan.DoesNotExist:
+                            messages.error(request, (
+                                "We couldn't find this subscription plan in our database. Please contact us for assistance."
+                                "Please contact us for assistance")
+                            )
+                            order.delete()
+                            return redirect(reverse('view_basket'))
+
             request.session['save_info'] = 'save-info' in request.POST
             return redirect(reverse('checkout_success', args=[order.order_number]))
 
@@ -135,8 +158,30 @@ def checkout_success(request, order_number):
     """
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
-    messages.success(request, f'Your order was successful. \
-        A confirmation \
+    
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        # Attach the user's profile to the order
+        order.user_profile = profile
+        order.save()
+
+         # Save the user's info
+        if save_info:
+            profile_data = {
+                'default_phone_number': order.phone_number,
+                'default_street_address1': order.street_address1,
+                'default_street_address2': order.street_address2,
+                'default_town_or_city': order.town_or_city,
+                'default_county': order.county,
+                'default_country': order.country,
+                'default_postcode': order.postcode,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
+
+    messages.success(request, f'Your order has been recieved. \
+        Your order number is {order_number}. A confirmation \
         email will be sent to {order.email}.')
 
     if 'basket' in request.session:
--- profiles/views.py
+++ profiles/views.py
@@ -4,6 +4,8 @@ from django.contrib import messages
 from .models import UserProfile
 from .forms import UserProfileForm
 
+from checkout.models import Order
+
 
 def profile(request):
     """ Display the user's profile. """
@@ -24,6 +26,20 @@ def profile(request):
         'form': form,
         'orders': orders,
         'on_profile_page': True,
+    }
+
+    return render(request, template, context)
+
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, f'Order confirmation for {order_number}.')
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order, 
+        'from_profile': True, 
     }
 
     return render(request, template, context)
