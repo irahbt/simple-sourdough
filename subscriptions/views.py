5730aff460a92e825a7920a0204306401cff6d38
irahbt <irah.bt@gmail.com>
2021-02-09 14:28:48 +0000

    Add subscription_webhook views and urls

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.http.response import HttpResponse
 from django.conf import settings
+from django.views.decorators.csrf import csrf_exempt
+
 
 from profiles.models import UserProfile
 
@@ -85,7 +87,41 @@ def subscription_cancel(request):
 
 def subscription_success(request):
 
-    if request.method == 'GET' and 'session_id' in request.GET:
+    # if request.method == 'GET' and 'session_id' in request.GET:
+    #     session = stripe.checkout.Session.retrieve(request.GET['session_id'],)
+    #     customer = UserProfile.objects.get(user=request.user)
+    #     customer.stripeid = session.customer
+    #     customer.membership = True
+    #     customer.cancel_at_period_end = False
+    #     customer.stripe_subscription_id = session.subscription
+    #     customer.save()
+
+    return render(request, 'subscriptions/subscription_success.html')
+
+
+@csrf_exempt
+def subscription_webhook(request):
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    endpoint_secret = settings.STRIPE_SUB_WH_SECRET
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, endpoint_secret
+        )
+    except ValueError as e:
+        # Invalid payload
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        return HttpResponse(status=400)
+
+    # Handle the checkout.session.completed event
+    if event['type'] == 'checkout.session.completed':
+        session = event['data']['object']
+
         session = stripe.checkout.Session.retrieve(request.GET['session_id'],)
         customer = UserProfile.objects.get(user=request.user)
         customer.stripeid = session.customer
@@ -94,4 +130,4 @@ def subscription_success(request):
         customer.stripe_subscription_id = session.subscription
         customer.save()
 
-    return render(request, 'subscriptions/subscription_success.html')
+    return HttpResponse(status=200)
