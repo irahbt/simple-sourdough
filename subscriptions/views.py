f8dda2b57aa3ae960614f72e2f0575783633f68a
irahbt <irah.bt@gmail.com>
2021-02-09 15:23:08 +0000

    Move subscription webhook view to webhooks.py

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -98,36 +98,3 @@ def subscription_success(request):
 
     return render(request, 'subscriptions/subscription_success.html')
 
-
-@csrf_exempt
-def subscription_webhook(request):
-    stripe.api_key = settings.STRIPE_SECRET_KEY
-    endpoint_secret = settings.STRIPE_SUB_WH_SECRET
-    payload = request.body
-    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
-    event = None
-
-    try:
-        event = stripe.Webhook.construct_event(
-            payload, sig_header, endpoint_secret
-        )
-    except ValueError as e:
-        # Invalid payload
-        return HttpResponse(status=400)
-    except stripe.error.SignatureVerificationError as e:
-        # Invalid signature
-        return HttpResponse(status=400)
-
-    # Handle the checkout.session.completed event
-    if event['type'] == 'checkout.session.completed':
-        session = event['data']['object']
-
-        session = stripe.checkout.Session.retrieve(request.GET['session_id'],)
-        customer = UserProfile.objects.get(user=request.user)
-        customer.stripeid = session.customer
-        customer.membership = True
-        customer.cancel_at_period_end = False
-        customer.stripe_subscription_id = session.subscription
-        customer.save()
-
-    return HttpResponse(status=200)
