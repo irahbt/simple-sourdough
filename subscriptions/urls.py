67a323639777a939e624058168da8cf649594c2b
irahbt <irah.bt@gmail.com>
2021-02-09 10:18:37 +0000

    Revert to old subscription views and templates

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -3,8 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.subscriptions, name='subscriptions'),
-    path('stripe_config/', views.stripe_config, name='stripe_config'),
-    path('create_checkout_session/', views.create_checkout_session, name='create_checkout_session'),
-    path('subscription_success/', views.subscription_success, name='subscription_success'),
-    path('subscription_cancel/', views.subscription_cancel, name='subscription_cancel'),
+    path('checkout_subscription/', views.checkout_subscription, name='checkout_subscription'),
 ]
--- subscriptions/views.py
+++ subscriptions/views.py
@@ -17,65 +17,46 @@ def subscriptions(request):
 
 
 @login_required
-@csrf_exempt
-def stripe_config(request):
-    if request.method == 'GET':
-        stripe_config = {'publicKey': settings.STRIPE_PUBLIC_KEY}
-        return JsonResponse(stripe_config, safe=False)
+def checkout_subscription(request):
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
 
+    try:
+        if request.user.userprofile.membership:
+            return redirect('profile')
+    except UserProfile.DoesNotExist:
+        pass
 
-@login_required
-@csrf_exempt
-def create_checkout_session(request):
-    if request.method == 'GET':
-        domain_url = 'https://8000-c258c6d1-b0ff-4fce-b6f4-eb72b8f7ec16.ws-eu03.gitpod.io/subscriptions/'
-        stripe.api_key = settings.STRIPE_SECRET_KEY
-        try:
-            checkout_session = stripe.checkout.Session.create(
-                client_reference_id=request.user.id if request.user.is_authenticated else None,
-                success_url=domain_url + 'subscription_success?session_id={CHECKOUT_SESSION_ID}/',
-                cancel_url=domain_url + 'subscription_cancel/',
-                payment_method_types=['card'],
-                mode='subscription',
-                line_items=[
-                    {
-                        'price': settings.STRIPE_PRICE_ID,
-                        'quantity': 1,
-                    }
-                ]
-            )
-            return JsonResponse({'sessionId': checkout_session['id']})
+    if request.method == 'POST':
+        stripe_customer = stripe.Customer.create(email=request.user.email, source=request.POST['stripeToken'])
+        subscription = 'price_1IECPRC0y3iCJrXqNx7gE55o'
+        if request.POST['subscription'] == 'yearly':
+            subscription = 'price_1IECPRC0y3iCJrXqNVUpbMIA'
 
-        except Exception as e:
-            return JsonResponse({'error': str(e)})
-
-
-@login_required
-def subscription_success(request):
-    # try:
-    #     if request.user.userprofile.membership:
-    #         return redirect('profile')
-    # except UserProfile.DoesNotExist:
-    #     pass
-
-    if request.user.is_authenticated:
-        stripe_customer_id = request.session.get('customer')
-        stripe_subscription_id = request.session.get('subscription')
+        subscription = stripe.Subscription.create(customer=stripe_customer.id,
+        items=[{'plan': subscription}])
 
         customer = UserProfile.objects.get(user=request.user)
-        customer.stripe_customer_id = stripe_customer_id
-        customer.stripe_subscription_id = stripe_subscription_id
+        customer.stripeid = stripe_customer.id
         customer.cancel_at_period_end = False
+        customer.stripe_subscription_id = subscription.id
         customer.membership = True
         customer.save()
 
-        return redirect('subscriptions')
+        return redirect('home')
 
-    return render(request, 'subscriptions/subscription_success.html')
+    else:
+        subscription = 'monthly'
+        price = 6
+        if request.method == 'GET' and 'subscription' in request.GET:
+            if request.GET['subscription'] == 'yearly':
+                subscription = 'yearly'
+                price = '60'
 
+        context = {
+            'subscription': subscription,
+            'price': price,
+            'stripe_public_key': stripe_public_key,
+        }
 
-@login_required
-def subscription_cancel(request):
-    return render(request, 'subscriptions/subscription_cancel.html')
-
-
+        return render(request, 'checkout/checkout_subscription.html', context)
