9c190328d18e39e966e4c01c9e6fa02c40a9fce3
irahbt <irah.bt@gmail.com>
2021-02-10 16:34:49 +0000

    Add recipe cards and content to recipes tempalate

--- recipes/models.py
+++ recipes/models.py
@@ -3,8 +3,11 @@ from django.db import models
 
 class Recipe(models.Model):
     title = models.CharField(max_length=255)
+    description = models.TextField(max_length=1000, null=True, blank=True)
     ingredients = models.TextField(null=True, blank=True)
     recipe_text = models.TextField(null=True, blank=True)
+    image_url = models.URLField(
+        max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
     added_date = models.DateField(auto_now_add=False, auto_now=False, blank=True)
 
