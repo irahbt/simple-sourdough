88f13e5c177708cc8b2df739086ddcf8c46d96a6
irahbt <irah.bt@gmail.com>
2021-02-15 15:48:41 +0000

    Add custom image widget to add_reipe

--- recipes/forms.py
+++ recipes/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from products.widgets import CustomClearableFileInput
 from .models import Ingredient, Recipe
 
 
@@ -18,6 +19,7 @@ class RecipeForm(forms.ModelForm):
         model = Recipe
         fields = '__all__'
 
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
