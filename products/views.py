f554ddd86e66b806b2f01948a7da8feb39c1573e
irahbt <irah.bt@gmail.com>
2021-02-15 12:21:10 +0000

    Add edit_product views, urls and template

--- products/views.py
+++ products/views.py
@@ -69,7 +69,7 @@ def add_product(request):
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Successfully added product!')
+            messages.success(request, 'Product Added Successful')
             return redirect(reverse('add_product'))
         else:
             messages.error(request, 'Add Product Failed. Please ensure the form is valid.')
@@ -79,6 +79,32 @@ def add_product(request):
     template = 'products/add_product.html'
     context = {
         'form': form,
+    }
+
+    return render(request, template, context)
+
+
+def edit_product(request, product_id):
+    """
+    Edit a product 
+    """
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Product Update Successful')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Update Product Failed. Please ensure the form is valid.')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
     }
 
     return render(request, template, context)
