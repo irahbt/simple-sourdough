043cb530fc60516b94e46951e1b6ea732f20d4f4
irahbt <irah.bt@gmail.com>
2021-02-15 12:12:36 +0000

    Finish add_product view, change product model added_date to auto_now_add

--- profiles/views.py
+++ profiles/views.py
@@ -28,6 +28,8 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Your information has been updated')
+        else:
+            messages.error(request, 'Update failed, Please check form is valid.')
 
     orders = profile.orders.all
     form = UserProfileForm(instance=profile)
