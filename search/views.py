bea15c400406bc449836c18699cef3eb68ef3a8f
irahbt <irah.bt@gmail.com>
2021-02-23 10:17:35 +0000

    Add docstrings and fix flake8 warnings

--- search/views.py
+++ search/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, reverse, redirect
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.db.models import Q
 
@@ -7,8 +7,15 @@ from recipes.models import Recipe
 
 
 def search(request):
-    """ 
-    A view to return product and recipe search requests
+    """
+
+    Retrieve all products and recipes
+    Check for q in request
+    Filter by query
+
+    Returns:
+    Search results page with search term and matching products and recipes
+
     """
     products = Product.objects.all()
     recipes = Recipe.objects.all()
@@ -19,7 +26,8 @@ def search(request):
             query = request.GET['q']
             if not query:
                 messages.error(
-                    request, "Hmm, you don't appear to have entered any search criteria. Please try again.")
+                    request, "Hmm, you don't appear to have entered any \
+                        search criteria. Please try again.")
                 return redirect('products')
 
         product_queries = Q(
