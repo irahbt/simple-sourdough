04de6ab3b016ebd4d923f3f90c24f3f1af6c3c67
irahbt <irah.bt@gmail.com>
2021-03-31 15:25:52 +0000

    Reset debug

--- starter/settings.py
+++ starter/settings.py
@@ -14,9 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = False
-
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['starter-sourdough.herokuapp.com', 'localhost']
 
