c78b7d5be9712728df7a1bef25334916378e5b5c
irahbt <irah.bt@gmail.com>
2021-03-31 11:06:24 +0000

    Revert basket views back to orginal stock control method

--- basket/views.py
+++ basket/views.py
@@ -1,3 +1,4 @@
+
 from django.shortcuts import (
     render, redirect, reverse,
     HttpResponse, get_object_or_404)
@@ -8,10 +9,8 @@ from products.models import Product
 
 def view_basket(request):
     """
-
     Returns:
     Basket contents page
-
     """
 
     return render(request, 'basket/basket.html')
@@ -19,13 +18,10 @@ def view_basket(request):
 
 def add_to_basket(request, item_id):
     """
-
     Add a quantity of the specified product to the shopping basket
     Prevent product amount from exceeding product inventory
-
     Returns:
     Url the request was made from
-
     """
 
     product = get_object_or_404(Product, pk=item_id)
@@ -35,24 +31,21 @@ def add_to_basket(request, item_id):
     basket = request.session.get('basket', {})
 
     if product.has_inventory and inventory >= quantity:
-        if item_id in list(basket.keys()):
-            basket[item_id] += quantity
-            if not product.inventory_updated:
-                product.remove_items_from_inventory(
-                    count=quantity, save=True)
-                product.inventory_updated = True
-            messages.success(
-                    request, f'{product.name} quantity has \
-                    been updated to {basket[item_id]}')
 
+        if item_id in list(basket.keys()):
+            if inventory > basket[item_id]:
+                basket[item_id] += quantity
+                messages.success(
+                        request, f'{product.name} quantity has \
+                        been updated to {basket[item_id]}')
+            else:
+                messages.error(request, f"Oh no, looks like there are only {inventory} {product.name} \
+                            left in stock, \
+                                please reduce your quantity to proceed.")
         else:
             basket[item_id] = quantity
-            if not product.inventory_updated:
-                product.remove_items_from_inventory(
-                    count=quantity, save=True)
-                product.inventory_updated = True
             messages.success(
-                    request, f'{product.name} has been added to your basket')
+                request, f'{product.name} has been added to your basket')
     else:
         messages.error(request, f"Oh no, looks like there are only {inventory} {product.name} \
                             left in stock, \
@@ -66,10 +59,8 @@ def update_basket(request, item_id):
     """
     Update the quantity of the specified product to the specified amount.
     Prevent product amount from exceeding product inventory.
-
     Returns:
     Basket contents page
-
     """
 
     product = get_object_or_404(Product, pk=item_id)
@@ -77,38 +68,20 @@ def update_basket(request, item_id):
     quantity = int(request.POST.get('quantity'))
     basket = request.session.get('basket', {})
 
-    for item_id, item_data in basket.items():
-        if quantity > 0:
-            if product.has_inventory:
-                inventory_total = inventory + item_data
-
-                if quantity <= inventory_total:
-                    basket[item_id] = quantity
-                    quantity_difference = quantity - item_data
-
-                    if not product.inventory_updated:
-                        product.remove_items_from_inventory(
-                            count=quantity_difference, save=True)
-                        product.inventory_updated = True
-                    messages.success(
-                        request, f'{product.name} quantity \
-                        has been updated to {basket[item_id]}')
-
-                else:
-                    messages.error(request, f"Oh no, looks like there are only {inventory_total} {product.name} \
-                            currently in stock, \
-                                please reduce your quantity to proceed.")
-            else:
-                messages.error(request, f"Oh no, looks like {product.name} \
-                    has sold out!")
-        else:
-            if not product.inventory_updated:
-                product.add_items_back_to_inventory(count=item_data, save=True)
-                product.inventory_updated = True
-
-            basket.pop(item_id)
+    if quantity > 0:
+        if quantity <= inventory:
+            basket[item_id] = quantity
             messages.success(
-                request, f'{product.name} has been removed from your basket')
+                    request, f'{product.name} quantity \
+                    has been updated to {basket[item_id]}')
+        else:
+            messages.error(request, f"Oh no, looks like there are only {inventory} {product.name} \
+                            left in stock, \
+                                please reduce your quantity to proceed.")
+    else:
+        basket.pop(item_id)
+        messages.success(
+            request, f'{product.name} has been removed from your basket')
 
     request.session['basket'] = basket
     return redirect(reverse('view_basket'))
@@ -116,21 +89,14 @@ def update_basket(request, item_id):
 
 def remove_from_basket(request, item_id):
     """
-
     Remove the item from the shopping basket
-
     Returns:
     Http Response
-
     """
 
     try:
         product = get_object_or_404(Product, pk=item_id)
         basket = request.session.get('basket', {})
-        for item_id, item_data in basket.items():
-            if not product.inventory_updated:
-                product.add_items_back_to_inventory(count=item_data, save=True)
-                product.inventory_updated = True
 
         basket.pop(item_id)
         messages.success(
